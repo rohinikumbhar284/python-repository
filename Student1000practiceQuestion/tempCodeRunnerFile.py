8760806d3bf0451064ab18adb2a8f225c49e4ed5
rohinikumbhar284 <rohinikumbhar284@gmail.com>
2025-08-04 12:46:07 +0530

    questions

--- Student1000practiceQuestion/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-ef exponent(base, exp):
-    result = 1
-    for _ in range(exp):
-        result *= base
-    return result
-
-# Example usage:
-print(exponent(2, 3))  # Output: 8
-print(exponent(5, 4))
